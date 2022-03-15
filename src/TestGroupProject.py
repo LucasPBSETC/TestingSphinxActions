c7dc7bff592435c3e22dafc44eea35d926f15c30
Lucas <lucas.pin-belloc.517@cranfield.ac.uk>
2022-03-15 11:19:20 +0000

    ell

---
+++ src/TestGroupProject.py
@@ -0,0 +1,17 @@
+
+def func1(arg1, arg2):
+    """Function that adds two arguments."""
+    return (arg1+arg2)
+
+def func2(arg):
+    """Function that transform a value into its negativ."""
+
+    return (-arg)
+
+if __name__ == "__main__":
+    a = 10
+    b = 667 
+
+    print(func1)
+    print("\n")
+    print(func2)
